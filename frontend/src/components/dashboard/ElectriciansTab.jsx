5c6a10d1ff9322a55581c99bd4940b124f3266b4
Vaishnavireddybhimireddy <bhimireddyvaishnavireddy@gmail.com>
2026-03-31 09:04:12 +0530

    Update ElectriciansTab.jsx

--- frontend/src/components/dashboard/ElectriciansTab.jsx
+++ frontend/src/components/dashboard/ElectriciansTab.jsx
@@ -265,7 +265,7 @@ const ElectriciansTab = () => {
           </div>
         ))}
       </div>
-
+      {/*Appointment model*/}
       {/* Appointment Modal */}
       {showModal && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
